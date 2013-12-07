278a97e044612ace8c2247bec75a300b4b044359
GeneChatham <genechatham@gmail.com>
2013-12-07 17:14:27 -0600

    Add dynamic api calls based on event. Creates Photo objects that are NOT unique.

--- instavent/app/models/event.rb
+++ instavent/app/models/event.rb
@@ -5,11 +5,14 @@ class Event < ActiveRecord::Base
 
   def get_photos
     key = ENV["CLIENT_ID"]
-    response = Unirest.get("https://api.instagram.com/v1/tags/mkshackathon/media/recent?client_id=#{key}")
-    photo = Photo.new
-    photo.event_id = self.id
-    photo.image = response.body["data"][2]["images"]["standard_resolution"]["url"]
-    photo.save    
+    response = Unirest.get("https://api.instagram.com/v1/tags/#{self.tag}/media/recent?client_id=#{key}")
+    response_array = response.body["data"]
+    response_array.each do |data|
+      photo = Photo.new
+      photo.event_id = self.id
+      photo.image = data["images"]["standard_resolution"]["url"]
+      photo.save
+    end    
   end
 
 end
