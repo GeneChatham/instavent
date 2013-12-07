80a4cf9524284bb755cedb44ea053e559a3b39f9
Andrew Krueger <krueger.andrew@gmail.com>
2013-12-06 19:24:30 -0600

    Initial commit

---
+++ instavent/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instavent::Application.config.secret_key_base = 'c04dfc538990c72e6452cfc83bbc19dd6b1fe3ba9b7f9750141ec6e272e34710e33a0b6cb76e7eed81ed529e2e078f3aa360ac6d179a93b4c8f2526ba2cacb03'
