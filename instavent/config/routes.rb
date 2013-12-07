f0aaf51d32b102387503cdf1ae1aa8ed604db55e
Andrew Krueger <krueger.andrew@gmail.com>
2013-12-06 20:10:04 -0600

    Add Event model and ActiveRecord parameters

--- instavent/config/routes.rb
+++ instavent/config/routes.rb
@@ -14,11 +14,17 @@ Instavent::Application.routes.draw do
 # Original routes created by Controller generation
 
   get "events/index"
+  
   get "events/new"
+  
   get "events/create"
+  
   get "events/show"
+  
   get "events/edit"
+  
   get "events/update"
+  
   get "events/destroy"
 
 end
