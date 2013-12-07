becbae357927276c656916d12037d250a63a7a99
mateoclarke <code@mateoclarke.com>
2013-12-07 11:51:06 -0600

    Find out basic methods in events controller.

--- instavent/app/controllers/events_controller.rb
+++ instavent/app/controllers/events_controller.rb
@@ -1,15 +1,24 @@
 class EventsController < ApplicationController
   def index
-    
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      redirect_to root_url
+    end
   end
 
   def show
+    id = params[:id]
+    @event = Event.find(id)
   end
 
   def edit
@@ -19,5 +28,10 @@ class EventsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+  def event_params
+    params.require(:event).permit(:tag)
   end
 end
