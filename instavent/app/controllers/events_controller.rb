4be54aad6a9abce40847a52fa886fffd1e06e9a4
Andrew Krueger <krueger.andrew@gmail.com>
2013-12-07 14:53:03 -0600

    Add pry, better errors gems. Add get_photo method functionality to Event model.

--- instavent/app/controllers/events_controller.rb
+++ instavent/app/controllers/events_controller.rb
@@ -19,6 +19,7 @@ class EventsController < ApplicationController
   def show
     id = params[:id]
     @event = Event.find(id)
+    @event.get_photos
   end
 
   def edit
--- instavent/app/models/event.rb
+++ instavent/app/models/event.rb
@@ -5,5 +5,11 @@ class Event < ActiveRecord::Base
 
   def get_photos
     key = ENV["CLIENT_ID"]
+    response = Unirest.get("https://api.instagram.com/v1/tags/mkshackathon/media/recent?client_id=#{key}")
+    photo = Photo.new
+    photo.event_id = self.id
+    photo.image = response.body["data"][2]["images"]["standard_resolution"]["url"]
+    photo.save    
   end
+
 end
