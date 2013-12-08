c2c14b8ac69ce3de12b81be967c27107293406fc
GeneChatham <genechatham@gmail.com>
2013-12-08 07:40:17 -0600

    Add ability to get multiple requests from instagram.  Set MAX_SHOW to limit results.

--- instavent/app/models/event.rb
+++ instavent/app/models/event.rb
@@ -3,16 +3,36 @@ class Event < ActiveRecord::Base
 
   validates :tag, presence: true
 
+  MAX_SHOW = 50
+
   def get_photos
     key = ENV["CLIENT_ID"]
-    response = Unirest.get("https://api.instagram.com/v1/tags/#{self.tag}/media/recent?client_id=#{key}")
-    response_array = response.body["data"]
-    response_array.each do |data|
-      photo = Photo.new
-      photo.event_id = self.id
-      photo.image = data["images"]["thumbnail"]["url"]
-      photo.save
-    end    
+    next_url = "https://api.instagram.com/v1/tags/#{self.tag}/media/recent?client_id=#{key}"
+    count = 0
+    full = nil
+    
+    while next_url && !full
+      response = Unirest.get(next_url)
+      next_url = response.body["pagination"]["next_url"]
+      response_array = response.body["data"]
+      
+      response_array.each do |data|
+        if count < MAX_SHOW
+          photo = Photo.new
+          photo.event_id = self.id
+          photo.image = data["images"]["thumbnail"]["url"]
+          photo.save
+          count += 1
+        else
+          full = true
+        end
+      end
+    
+    end
+
+  
   end
+
+
 
 end
