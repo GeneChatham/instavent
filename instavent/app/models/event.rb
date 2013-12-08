5ac2c30acd456d612c33947579b9c25f71ad93f3
GeneChatham <genechatham@gmail.com>
2013-12-08 13:31:30 -0600

    Fix time-optional bug, add photo_save method, refactored.

--- instavent/app/models/event.rb
+++ instavent/app/models/event.rb
@@ -12,46 +12,59 @@ class Event < ActiveRecord::Base
     key = ENV["CLIENT_ID"]
     next_url = "https://api.instagram.com/v1/tags/#{self.tag}/media/recent?client_id=#{key}"
     count = 0
-    full = nil
-    human_start_time = Chronic.parse(self.start_time)
-    human_end_time = Chronic.parse(self.end_time)
+    event_start_time = Chronic.parse(self.start_time)
+    event_end_time = Chronic.parse(self.end_time)
     @unix_created_time = 0
+    event_time_present = self.start_time.present? && self.end_time.present?
 
 
-    while next_url && !full
+    while next_url && count < MAX_SHOW
       response = Unirest.get(next_url)
       next_url = response.body["pagination"]["next_url"]
       response_array = response.body["data"]
-      
-      response_array.each do |data|
-        if count < MAX_SHOW
-          if self.start_time.present? && self.end_time.present?
+
+      # if event_time_present
+      #   insta_array_last = response.body["data"].last
+      #   earliest_time = insta_array_last["created_time"]
+      # if earliest < event_end_time
+      if event_time_present
+        response_array.each do |data|
+          if count < MAX_SHOW
             @unix_created_time = data["created_time"]
             human_time = Time.at(@unix_created_time.to_i)
-            if human_time > human_start_time && human_time < human_end_time
+            if human_time > event_start_time && human_time < event_end_time
               photo_save(data)
               count += 1
             end
-          
           else
-            photo_save(data)
-            count += 1            
-          end
-        else
-          full = true
+            break
+          end         
         end
+      else
+        response_array.each do |data|
+          if count < MAX_SHOW
+            @unix_created_time = data["created_time"]
+            photo_save(data)
+            count += 1
+          else
+            break
+          end
+        end         
       end
+      # end
     end
   end
 
   def photo_save(data)
     photo = Photo.new
     photo.event_id = self.id
-    photo.image = data["images"]["thumbnail"]["url"]
+    photo.image = data["images"]["standard_resolution"]["url"]
     photo.time = @unix_created_time.to_i
     photo.save
 
   end
+
+
 
 end
       
